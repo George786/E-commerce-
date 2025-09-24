8281391b4f9e9eaa9b45c12943f0066501547481
Subhash <hndsmby123@gmail.com>
2025-09-24 15:02:20 +0530

    Fixed the nodemailer

--- src/types/nodemailer.d.ts
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-// Declare nodemailer module to satisfy TypeScript if types are missing
-declare module 'nodemailer' {
-	const nodemailer: any
-	export default nodemailer
-}
